5a37d33de09a76da28f8ca8ff53fd25bc5578cd7
jwirjono <jwirjono@gmail.com>
2026-04-30 23:26:29 +0700

    navbar fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -58,7 +58,7 @@ export default function Navbar({ onSearchOpen }: NavbarProps) {
     >
       <Link 
         href="/" 
-        className="relative z-[90] w-12 h-8"
+        className="relative z-[90] w-12 h-16"
         onClick={() => setIsMobileMenuOpen(false)}
       >
               <Image
