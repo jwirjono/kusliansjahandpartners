ab4c1d850b6563674a04baa2bbdc7403c385050c
AnakCinaHoki <work.kevinkristofan@gmail.com>
2026-07-26 17:15:22 +0700

    Update Categories & Add Project SHHR Church Complex

--- public/images/projects/index.ts
+++ public/images/projects/index.ts
@@ -1,92 +1,111 @@
-import mCover from './1/merdekaCover.png'
-import a1 from './1/merdeka1.png'
-import a2 from './1/merdeka2.jpg'
-import b1 from './2/1.png'
-import b2 from './2/2.png'
-import b3 from './2/3.png'
-import b4 from './2/4.png'
-import b5 from './2/5.png'
-import b6 from './2/6.png'
-import bCover from './2/cover.png'
-import c1 from './3/1.png'
-import c2 from './3/2.png'
-import c3 from './3/3.png'
-import c4 from './3/4.png'
-import c5 from './3/5.png'
-import cCover from './3/cover.png'
-import d1 from './4/1.png'
-import d2 from './4/2.png'
-import dCover from './4/cover.png'
+import mCover from "./1/merdekaCover.png";
+import a1 from "./1/merdeka1.png";
+import a2 from "./1/merdeka2.jpg";
+import b1 from "./2/1.png";
+import b2 from "./2/2.png";
+import b3 from "./2/3.png";
+import b4 from "./2/4.png";
+import b5 from "./2/5.png";
+import b6 from "./2/6.png";
+import bCover from "./2/cover.png";
+import c1 from "./3/1.png";
+import c2 from "./3/2.png";
+import c3 from "./3/3.png";
+import c4 from "./3/4.png";
+import c5 from "./3/5.png";
+import cCover from "./3/cover.png";
+import d1 from "./4/1.png";
+import d2 from "./4/2.png";
+import dCover from "./4/cover.png";
 
-import e1 from './5/1.png'
-import e2 from './5/2.png'
-import e3 from './5/3.png'
-import e4 from './5/4.png'
-import e5 from './5/5.png'
-import e6 from './5/6.png'
-import e7 from './5/7.png'
-import e8 from './5/8.png'
-import e9 from './5/9.jpg'
-import e10 from './5/10.jpg'
-import e11 from './5/11.jpg'
-import e12 from './5/12.jpg'
-import e13 from './5/13.jpg'
-import eCover from './5/cover.png'
+import e1 from "./5/1.png";
+import e2 from "./5/2.png";
+import e3 from "./5/3.png";
+import e4 from "./5/4.png";
+import e5 from "./5/5.png";
+import e6 from "./5/6.png";
+import e7 from "./5/7.png";
+import e8 from "./5/8.png";
+import e9 from "./5/9.jpg";
+import e10 from "./5/10.jpg";
+import e11 from "./5/11.jpg";
+import e12 from "./5/12.jpg";
+import e13 from "./5/13.jpg";
+import eCover from "./5/cover.png";
 
-import f1 from './6/1.png'
-import f2 from './6/2.png'
-import f3 from './6/3.png'
-import f4 from './6/4.png'
-import f5 from './6/5.png'
-import f6 from './6/6.png'
-import f7 from './6/7.png'
-import f8 from './6/8.png'
-import fCover from './6/cover.png'
+import f1 from "./6/1.png";
+import f2 from "./6/2.png";
+import f3 from "./6/3.png";
+import f4 from "./6/4.png";
+import f5 from "./6/5.png";
+import f6 from "./6/6.png";
+import f7 from "./6/7.png";
+import f8 from "./6/8.png";
+import fCover from "./6/cover.png";
 
-import g1 from './7/1.png'
-import g2 from './7/2.png'
-import g3 from './7/3.png'
-import g4 from './7/4.png'
-import g5 from './7/5.png'
-import g6 from './7/6.png'
-import g7 from './7/7.png'
-import g8 from './7/8.png'
-import g9 from './7/9.png'
-import g10 from './7/10.png'
-import g11 from './7/11.png'
-import g12 from './7/12.png'
-import g13 from './7/13.png'
-import g14 from './7/14.png'
-import g15 from './7/15.png'
-import gCover from './7/cover.png'
+import g1 from "./7/1.png";
+import g2 from "./7/2.png";
+import g3 from "./7/3.png";
+import g4 from "./7/4.png";
+import g5 from "./7/5.png";
+import g6 from "./7/6.png";
+import g7 from "./7/7.png";
+import g8 from "./7/8.png";
+import g9 from "./7/9.png";
+import g10 from "./7/10.png";
+import g11 from "./7/11.png";
+import g12 from "./7/12.png";
+import g13 from "./7/13.png";
+import g14 from "./7/14.png";
+import g15 from "./7/15.png";
+import gCover from "./7/cover.png";
+
+import h1 from "./8/1.png";
+import h2 from "./8/2.png";
+import h3 from "./8/3.png";
+import h4 from "./8/4.png";
+import h5 from "./8/5.png";
+import h6 from "./8/6.png";
+import h7 from "./8/7.png";
+import h8 from "./8/8.png";
+import h9 from "./8/9.png";
+import h10 from "./8/10.png";
+import h11 from "./8/11.png";
+import h12 from "./8/12.png";
+import h13 from "./8/13.png";
+import hCover from "./8/cover.png";
 
 export const images = {
   merdeka: {
     image: a1,
-    gallery: [a1,a2]
+    gallery: [a1, a2],
   },
-  mandiri:{
+  mandiri: {
     image: bCover,
-    gallery: [b1,b2,b3,b4,b5,b6]
+    gallery: [b1, b2, b3, b4, b5, b6],
   },
-  parahyanganACET:{
+  parahyanganACET: {
     image: cCover,
-    gallery:[c1,c2,c3,c4,c5]
+    gallery: [c1, c2, c3, c4, c5],
   },
-  smpn22:{
+  smpn22: {
     image: dCover,
-    gallery:[d1,d2]
+    gallery: [d1, d2],
   },
-  slemanCulturePark:{
+  slemanCulturePark: {
     image: eCover,
-    gallery:[e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13]
+    gallery: [e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13],
   },
-  suniarajaDesignCenter:{
+  suniarajaDesignCenter: {
     image: fCover,
-    gallery:[f1,f2,f3,f4,f5,f6,f7,f8]
+    gallery: [f1, f2, f3, f4, f5, f6, f7, f8],
   },
-  GubengTransitHub:{
+  GubengTransitHub: {
     image: gCover,
-    gallery:[g1,g2,g3,g4,g5,g6,g7,g8,g9,g10,g11,g12,g13,g14,g15]
+    gallery: [g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12, g13, g14, g15],
   },
-}
+  SHHRChurchComplex: {
+    image: hCover,
+    gallery: [h1, h2, h3, h4, h5, h6, h7, h8, h9, h10, h11, h12, h13],
+  },
+};
